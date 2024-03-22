6412850fe071cb51a200915138d0c5d21cfb0c7b
Graham Morrison <40895085+morpole@users.noreply.github.com>
2024-03-22 18:08:24 +0000

    Add files via upload

---
+++ app.js
@@ -0,0 +1,30 @@
+
+const express = require('express');
+
+const app = express();
+
+
+app.set('view engine', 'ejs');
+
+app.listen(3000);
+
+app.get('/', (req,res) => { 
+    const blogs = [
+        {title: 'Yoshi finds eggs', snippet: 'Lorem ipsum deddu makes fun of it all atall atall '},
+        {title: 'Mario finds stars', snippet: 'Lorem ipsum deddu makes fun of it all atall atall '},
+        {title: 'How to defeat bowser', snippet: 'Lorem ipsum deddu makes fun of it all atall atall '}
+    ]
+    res.render('index', {title: 'Home', blogs});
+});
+
+app.get('/about', (req,res) => { 
+    res.render('about' , {title: 'About'});
+});
+
+app.get('/blogs/create', (rq,res) => {
+    res.render('create', {title: 'Create a new blog'});
+})
+
+app.use((req,res) => { 
+    res.status(404).render('404', {title: '404'});
+});
